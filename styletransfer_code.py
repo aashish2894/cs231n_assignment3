4766e04bf8170ee8ca6a8b01aad6664acef42923
aashish2894 <anshu.2894@gmail.com>
2018-01-03 23:43:17 -0600

    Code Added

---
+++ styletransfer_code.py
@@ -0,0 +1,250 @@
+from scipy.misc import imread, imresize
+import numpy as np
+
+from scipy.misc import imread
+import matplotlib.pyplot as plt
+
+# Helper functions to deal with image preprocessing
+from cs231n.image_utils import load_image, preprocess_image, deprocess_image
+
+
+def get_session():
+    """Create a session that dynamically allocates memory."""
+    # See: https://www.tensorflow.org/tutorials/using_gpu#allowing_gpu_memory_growth
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    session = tf.Session(config=config)
+    return session
+
+def rel_error(x,y):
+    return np.max(np.abs(x - y) / (np.maximum(1e-8, np.abs(x) + np.abs(y))))
+
+# Older versions of scipy.misc.imresize yield different results
+# from newer versions, so we check to make sure scipy is up to date.
+def check_scipy():
+    import scipy
+    vnum = int(scipy.__version__.split('.')[1])
+    assert vnum >= 16, "You must install SciPy >= 0.16.0 to complete this notebook."
+
+check_scipy()
+
+
+from cs231n.classifiers.squeezenet import SqueezeNet
+import tensorflow as tf
+
+tf.reset_default_graph() # remove all existing variables in the graph 
+sess = get_session() # start a new Session
+
+# Load pretrained SqueezeNet model
+SAVE_PATH = 'cs231n/datasets/squeezenet.ckpt'
+# if not os.path.exists(SAVE_PATH):
+#     raise ValueError("You need to download SqueezeNet!")
+model = SqueezeNet(save_path=SAVE_PATH, sess=sess)
+
+# Load data for testing
+content_img_test = preprocess_image(load_image('styles/tubingen.jpg', size=192))[None]
+style_img_test = preprocess_image(load_image('styles/starry_night.jpg', size=192))[None]
+answers = np.load('style-transfer-checks-tf.npz')
+
+def content_loss(content_weight, content_current, content_original):
+    shapes = tf.shape(content_current)
+    F_l = tf.reshape(content_current, [shapes[1], shapes[2]*shapes[3]])
+    P_l = tf.reshape(content_original,[shapes[1], shapes[2]*shapes[3]])
+    loss = content_weight*(tf.reduce_sum((F_l-P_l)**2))
+    return loss
+
+def content_loss_test(correct):
+    content_layer = 3
+    content_weight = 6e-2
+    c_feats = sess.run(model.extract_features()[content_layer], {model.image: content_img_test})
+    bad_img = tf.zeros(content_img_test.shape)
+    feats = model.extract_features(bad_img)[content_layer]
+    student_output = sess.run(content_loss(content_weight, c_feats, feats))
+    error = rel_error(correct, student_output)
+    print('Maximum error is {:.3f}'.format(error))
+
+content_loss_test(answers['cl_out'])
+
+
+def gram_matrix(features, normalize=True):
+    """
+    Compute the Gram matrix from features.
+    
+    Inputs:
+    - features: Tensor of shape (1, H, W, C) giving features for
+      a single image.
+    - normalize: optional, whether to normalize the Gram matrix
+        If True, divide the Gram matrix by the number of neurons (H * W * C)
+    
+    Returns:
+    - gram: Tensor of shape (C, C) giving the (optionally normalized)
+      Gram matrices for the input image.
+    """
+    shapes = tf.shape(features)
+    F_l = tf.reshape(features, shape=[shapes[1]*shapes[2],shapes[3]])
+    gram = tf.matmul(tf.transpose(F_l),F_l)
+    if normalize==True:
+        gram /= tf.cast(shapes[1]*shapes[2]*shapes[3],tf.float32)
+    return gram
+
+def gram_matrix_test(correct):
+    gram = gram_matrix(model.extract_features()[5])
+    student_output = sess.run(gram, {model.image: style_img_test})
+    error = rel_error(correct, student_output)
+    print('Maximum error is {:.3f}'.format(error))
+
+gram_matrix_test(answers['gm_out'])
+
+def style_loss(feats, style_layers, style_targets, style_weights):
+    style_loss = tf.constant(0.0)
+    for i in range(len(style_layers)):
+        current_im_gram = gram_matrix(feats[style_layers[i]])
+        style_loss += style_weights[i]*tf.reduce_sum((current_im_gram-style_targets[i])**2)
+    return style_loss
+
+def style_loss_test(correct):
+    style_layers = [1, 4, 6, 7]
+    style_weights = [300000, 1000, 15, 3]
+    feats = model.extract_features()
+    style_target_vars = []
+    for idx in style_layers:
+        style_target_vars.append(gram_matrix(feats[idx]))
+    style_targets = sess.run(style_target_vars,
+                             {model.image: style_img_test})                         
+    s_loss = style_loss(feats, style_layers, style_targets, style_weights)
+    student_output = sess.run(s_loss, {model.image: content_img_test})
+    error = rel_error(correct, student_output)
+    print('Error is {:.3f}'.format(error))
+
+style_loss_test(answers['sl_out'])
+
+def tv_loss(img, tv_weight):
+    w_variance = tf.reduce_sum((img[:,:,1:,:] - img[:,:,:-1,:])**2)
+    h_variance = tf.reduce_sum((img[:,1:,:,:] - img[:,:-1,:,:])**2)
+    loss = tv_weight*(w_variance+h_variance)
+    return loss
+
+def tv_loss_test(correct):
+    tv_weight = 2e-2
+    t_loss = tv_loss(model.image, tv_weight)
+    student_output = sess.run(t_loss, {model.image: content_img_test})
+    error = rel_error(correct, student_output)
+    print('Error is {:.3f}'.format(error))
+
+tv_loss_test(answers['tv_out'])
+
+def style_transfer(content_image, style_image, image_size, style_size, content_layer, content_weight, style_layers, style_weights, tv_weight, init_random = False):
+    content_img = preprocess_image(load_image(content_image, size=image_size))
+    feats = model.extract_features(model.image)
+    content_target = sess.run(feats[content_layer],
+                              {model.image: content_img[None]})
+    # Extract features from the style image
+    style_img = preprocess_image(load_image(style_image, size=style_size))
+    style_feat_vars = [feats[idx] for idx in style_layers]
+    style_target_vars = []
+    # Compute list of TensorFlow Gram matrices
+    for style_feat_var in style_feat_vars:
+        style_target_vars.append(gram_matrix(style_feat_var))
+    # Compute list of NumPy Gram matrices by evaluating the TensorFlow graph on the style image
+    style_targets = sess.run(style_target_vars, {model.image: style_img[None]})
+    # Initialize generated image to content image
+    if init_random:
+        img_var = tf.Variable(tf.random_uniform(content_img[None].shape, 0, 1), name="image")
+    else:
+        img_var = tf.Variable(content_img[None], name="image")
+    # Extract features on generated image
+    feats = model.extract_features(img_var)
+    # Compute loss
+    c_loss = content_loss(content_weight, feats[content_layer], content_target)
+    s_loss = style_loss(feats, style_layers, style_targets, style_weights)
+    t_loss = tv_loss(img_var, tv_weight)
+    loss = c_loss + s_loss + t_loss
+    # Set up optimization hyperparameters
+    initial_lr = 3.0
+    decayed_lr = 0.1
+    decay_lr_at = 180
+    max_iter = 200
+    # Create and initialize the Adam optimizer
+    lr_var = tf.Variable(initial_lr, name="lr")
+    # Create train_op that updates the generated image when run
+    with tf.variable_scope("optimizer") as opt_scope:
+        train_op = tf.train.AdamOptimizer(lr_var).minimize(loss, var_list=[img_var])
+    # Initialize the generated image and optimization variables
+    opt_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope=opt_scope.name)
+    sess.run(tf.variables_initializer([lr_var, img_var] + opt_vars))
+    # Create an op that will clamp the image values when run
+    clamp_image_op = tf.assign(img_var, tf.clip_by_value(img_var, -1.5, 1.5))
+    f, axarr = plt.subplots(1,2)
+    axarr[0].axis('off')
+    axarr[1].axis('off')
+    axarr[0].set_title('Content Source Img.')
+    axarr[1].set_title('Style Source Img.')
+    axarr[0].imshow(deprocess_image(content_img))
+    axarr[1].imshow(deprocess_image(style_img))
+    plt.show()
+    plt.figure()
+    # Hardcoded handcrafted 
+    for t in range(max_iter):
+        # Take an optimization step to update img_var
+        sess.run(train_op)
+        if t < decay_lr_at:
+            sess.run(clamp_image_op)
+        if t == decay_lr_at:
+            sess.run(tf.assign(lr_var, decayed_lr))
+        if t % 100 == 0:
+            print('Iteration {}'.format(t))
+            img = sess.run(img_var)
+            plt.imshow(deprocess_image(img[0], rescale=True))
+            plt.axis('off')
+            plt.show()
+    print('Iteration {}'.format(t))
+    img = sess.run(img_var)        
+    plt.imshow(deprocess_image(img[0], rescale=True))
+    plt.axis('off')
+    plt.show()
+
+# Composition VII + Tubingen
+params1 = {
+    'content_image' : 'styles/tubingen.jpg',
+    'style_image' : 'styles/composition_vii.jpg',
+    'image_size' : 192,
+    'style_size' : 512,
+    'content_layer' : 3,
+    'content_weight' : 5e-2, 
+    'style_layers' : (1, 4, 6, 7),
+    'style_weights' : (20000, 500, 12, 1),
+    'tv_weight' : 5e-2
+}
+
+style_transfer(**params1)
+
+# Scream + Tubingen
+params2 = {
+    'content_image':'styles/tubingen.jpg',
+    'style_image':'styles/the_scream.jpg',
+    'image_size':192,
+    'style_size':224,
+    'content_layer':3,
+    'content_weight':3e-2,
+    'style_layers':[1, 4, 6, 7],
+    'style_weights':[200000, 800, 12, 1],
+    'tv_weight':2e-2
+}
+
+style_transfer(**params2)
+
+# Starry Night + Tubingen
+params3 = {
+    'content_image' : 'styles/tubingen.jpg',
+    'style_image' : 'styles/starry_night.jpg',
+    'image_size' : 192,
+    'style_size' : 192,
+    'content_layer' : 3,
+    'content_weight' : 6e-2,
+    'style_layers' : [1, 4, 6, 7],
+    'style_weights' : [300000, 1000, 15, 3],
+    'tv_weight' : 2e-2
+}
+
+style_transfer(**params3)
+
